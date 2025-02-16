4c1f9f20ce7957b5037192cd56908b5218a0d909
Thiago Martins <thiago_valentim1@hotmail.com>
2025-02-16 09:09:02 -0300

    chore: add first e2e stubbed test

---
+++ test/get-address-coordinates.e2e-spec.ts
@@ -0,0 +1,65 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+import { http, HttpResponse } from 'msw';
+import { setupServer, SetupServerApi } from 'msw/node';
+
+describe('Get GeoCode Address', () => {
+  let app: INestApplication;
+  let mockServer: SetupServerApi;
+
+  const handlers = [
+    // Intercept "GET 'https://maps.googleapis.com/maps/api/geocode" requests...
+    http.get(
+      'https://maps.googleapis.com/maps/api/geocode/json?key-test&address=1600+Amphitheatre+Parkway,+Mountain+View,+CA',
+      () => {
+        // ...and respond to them using this JSON response.
+        return HttpResponse.json([
+          {
+            results: [
+              {
+                geometry: {
+                  location: {
+                    lat: 37.4224082,
+                    lng: -122.0856086,
+                  },
+                },
+              },
+            ],
+          },
+        ]);
+      },
+    ),
+  ];
+
+  beforeAll(() => {
+    mockServer = setupServer(...handlers);
+    mockServer.listen();
+  });
+
+  afterAll(() => {
+    mockServer.close();
+  });
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('returns the latitude/longitude for a given address', () => {
+    return request(app.getHttpServer())
+      .get(
+        '/addresses/geo-code?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA',
+      )
+      .expect(200)
+      .expect({
+        latitude: 37.4224082,
+        longitude: -122.0856086,
+      });
+  });
+});

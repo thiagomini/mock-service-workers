3c157d54a255bbd974926063f25eb945b753d17c
Thiago Martins <thiago_valentim1@hotmail.com>
2025-03-02 09:34:28 -0300

    feat(domain): validate coordinates

--- src/address/domain/coordinates.ts
+++ src/address/domain/coordinates.ts
@@ -3,7 +3,22 @@ export class Coordinates {
   public readonly longitude: number;
 
   constructor(lat: number, lng: number) {
+    if (!Coordinates.isValid(lat, lng)) {
+      throw new Error(`Invalid coordinates: lat=${lat}, lng=${lng}`);
+    }
+
     this.latitude = lat;
     this.longitude = lng;
+  }
+
+  static isValid(lat: number, lng: number): boolean {
+    return (
+      typeof lat === 'number' &&
+      typeof lng === 'number' &&
+      lat >= -90 &&
+      lat <= 90 &&
+      lng >= -180 &&
+      lng <= 180
+    );
   }
 }
